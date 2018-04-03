4e24870347ed4581dc4384ebd496f79923650310
Joanne <thecodingdiva@gmail.com>
2018-04-03 08:38:57 -0400

    program that asks user for 2 numbers and displays the greater of 2 numbers

--- GreaterNumber/Program.cs
+++ GreaterNumber/Program.cs
@@ -9,7 +9,35 @@ namespace GreaterNumber
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            Num();
         }
+
+        static void Num()
+        {
+            double input1, input2;
+            Console.WriteLine("What is your first number: ");
+            input1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your second number: ");
+            input2 = Convert.ToDouble(Console.ReadLine());
+
+            if (input1 > input2)
+            {
+                Console.WriteLine("The max number is " + input1);
+            }
+
+            else if(input1 < input2)
+            {
+                Console.WriteLine("The max number is " + input2);
+            }
+
+            else
+            {
+                Console.WriteLine("The numbers are equal");
+            }
+        }
+
+        
     }
 }
